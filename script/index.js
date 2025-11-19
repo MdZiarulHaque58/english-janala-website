4bc302cd3acf200fd20c5b254d4feb85fd13b66b
Md Ziarul Haque <mdziarulhaque555@gmail.com>
2025-11-19 12:34:23 +0600

    added lesson functionality

---
+++ script/index.js
@@ -0,0 +1,21 @@
+const loadLessons = () => {
+    fetch("https://openapi.programming-hero.com/api/levels/all") //promise of response
+    .then((res) => res.json()) //promise of json data
+    .then((json) => displayLesson(json.data)); 
+}
+
+const displayLesson = (lessons) => {
+    const levelContainer=document.getElementById("level-container");
+    levelContainer.innerHTML= "";
+
+    for(let lesson of lessons){
+        console.log(lesson);
+        const btnDiv = document.createElement("div");
+        btnDiv.innerHTML = `    
+        <button class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i>Lesson - ${lesson.level_no}</button>
+        `;
+    levelContainer.append(btnDiv);
+    }
+};
+
+loadLessons();
